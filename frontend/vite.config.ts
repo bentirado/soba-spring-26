436506484a10665b00204e3212c55f4bc2bfd2c6
Wahid Haidari <68567891+Wahid-Haidari@users.noreply.github.com>
2026-03-25 19:16:11 -0500

    Make components for charts and stat cards (#44)
    Co-authored-by: bentirado <benjam.tirado@gmail.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
